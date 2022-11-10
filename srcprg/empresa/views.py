e141a4c827bfc5725b439586698fefa5ceeee68f
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2022-11-10 00:20:58 -0300

    7-create-db-for-applicatipn-enpresa

--- srcprg/empresa/views.py
+++ srcprg/empresa/views.py
@@ -1,6 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from . models import Tecnologias, Empresa
+
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
-# Create your views here.
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == "GET":
+        techs = Tecnologias.objects.all()
+        return render(request, 'nova_empresa.html', {'techs': techs})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        caracteristicas = request.POST.get('caracteristicas')
+        # getlist porque vamos receber mais de um parametro
+        tecnologias = request.POST.getlist('tecnologias')
+        # vamos receber uma picture
+        logo = request.FILES.get('logo')
+
+        # strip retira os espaços em branco
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)): # noqa
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/home/nova_empresa')
+
+        if logo.size > 100_000_000:  # logo não pode ser maior que 10MBits
+            messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/home/nova_empresa')
+
+        if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+            messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/home/nova_empresa')
+
+        empresa = Empresa(
+            logo=logo,
+            nome=nome,
+            email=email,
+            cidade=cidade,
+            endereco=endereco,
+            nicho_mercado=nicho,
+            caracteristica_empresa=caracteristicas
+        )
+
+        empresa.save()
+        empresa.tecnologias.add(*tecnologias)
+        empresa.save()
+        messages.add_message(
+            request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
+        return redirect('/home/empresas')
+
+
+def empresas(request):
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    return render(request, 'empresa.html', {'empresas': empresas, 'tecnologias': tecnologias})
+
+
+def excluir_empresa(request, id):
+    empresa = Empresa.objects.get(id=id)
+    empresa.delete()
+    messages.add_message(
+            request, constants.SUCCESS, 'Empresa excluida com sucesso')
+    return redirect('/home/empresas')
